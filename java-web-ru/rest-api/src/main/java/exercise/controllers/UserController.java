83a9ceea971c70396ed35d5f493abe54620c2816
@hexlet/cli <support@hexlet.io>
2022-12-12 19:36:47 +0000

    submit java-web-ru/rest-api

--- java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
+++ java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
@@ -1,8 +1,11 @@
 package exercise.controllers;
 
+import io.javalin.core.validation.BodyValidator;
+import io.javalin.core.validation.Validator;
 import io.javalin.http.Context;
 import io.javalin.apibuilder.CrudHandler;
 import io.ebean.DB;
+
 import java.util.List;
 
 import exercise.domain.User;
@@ -19,7 +22,9 @@ public class UserController implements CrudHandler {
         String jsonUsers = DB.json().toJson(users);
         ctx.json(jsonUsers);
         // END
-    };
+    }
+
+    ;
 
     public void getOne(Context ctx, String id) {
 
@@ -28,16 +33,25 @@ public class UserController implements CrudHandler {
         String jsonUser = DB.json().toJson(user);
         ctx.json(jsonUser);
         // END
-    };
+    }
+
+    ;
 
     public void create(Context ctx) {
 
         // BEGIN
-        String jsonUser = ctx.body();
-        User user = DB.json().toBean(User.class, jsonUser);
+        User user = ctx.bodyValidator(User.class)
+                .check(u -> !u.getFirstName().isEmpty() , "err1")
+                .check(u -> !u.getLastName().isEmpty() , "err2")
+                .check(u -> EmailValidator.getInstance().isValid(u.getEmail()), "err3")
+                .check(u -> u.getPassword().length() > 4 , "err4")
+                .check(u -> StringUtils.isNumeric(u.getPassword()), "err5")
+                .get();
         user.save();
         // END
-    };
+    }
+
+    ;
 
     public void update(Context ctx, String id) {
         // BEGIN
@@ -48,13 +62,18 @@ public class UserController implements CrudHandler {
                 .set("firstName", user.getFirstName())
                 .set("lastName", user.getLastName())
                 .set("email", user.getEmail())
+                .set("password", user.getPassword())
                 .update();
         // END
-    };
+    }
+
+    ;
 
     public void delete(Context ctx, String id) {
         // BEGIN
         new QUser().id.equalTo(Integer.parseInt(id)).delete();
         // END
-    };
+    }
+
+    ;
 }
