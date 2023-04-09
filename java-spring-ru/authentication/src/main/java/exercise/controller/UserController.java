c7f622566bc10c1bb4b840cc319569e8634ff628
@hexlet/cli <support@hexlet.io>
2023-04-09 20:31:07 +0000

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/java/exercise/controller/UserController.java
@@ -0,0 +1,41 @@
+package exercise.controller;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Кодировщик BCrypt
+    // Используйте для хеширования пароля
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @GetMapping(path = "")
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    Iterable<User> createUser(@RequestBody User newUser) {
+        String encoded = encoder.encode(newUser.getPassword());
+        newUser.setPassword(encoded);
+        userRepository.save(newUser);
+
+        return userRepository.findAll();
+    }
+    // END
+}
